67a43cab41f7c4bd6ecddeb65a99a0a1659f3be2
Aleksandar Aleksiev <59371916+AleksievAleksandar@users.noreply.github.com>
2020-02-11 16:23:46 +0200

    Update Main.cpp

--- Exam 1 (15 July 2018, 09:00)/Format Lines/Main.cpp
+++ Exam 1 (15 July 2018, 09:00)/Format Lines/Main.cpp
@@ -1,1 +1,44 @@
 #include <iostream>
+#include <sstream>
+
+int main()
+{
+	std::string getUserInputStr;
+	std::getline(std::cin, getUserInputStr);
+
+	std::stringstream infoStream;
+	
+	while (getUserInputStr != "###")
+	{
+		infoStream << getUserInputStr << " ";
+		std::getline(std::cin, getUserInputStr);
+	}
+
+	getUserInputStr.clear();
+	std::getline(std::cin, getUserInputStr);
+	size_t width = std::stoi(getUserInputStr);
+
+	std::string word;
+	size_t sizeWords = 0;
+	std::ostringstream toPrint;
+	while (!infoStream.eof())
+	{
+		infoStream >> word;
+		sizeWords += word.size();
+
+		if (sizeWords <= width)
+		{
+			toPrint << word;
+		}
+		else
+		{
+			toPrint << "\n";
+		}
+	}
+
+	std::cout << toPrint.str();
+
+	int a;
+	std::cin >> a;
+	return 0;
+}
